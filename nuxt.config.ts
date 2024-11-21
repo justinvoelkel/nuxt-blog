9cbc6dff55e50d658dd7ba808a92b251af55f030
Justin Voelkel <justin.voelkel@sourcetoad.com>
2024-11-21 14:19:09 -0500

    add shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: {enabled: true},
   ssr: true,
-  modules: ['@nuxt/content', '@nuxt/eslint'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode',
+  ],
 });

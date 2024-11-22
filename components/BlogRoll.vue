21cf7e3d673b4968bd22c3b380e780095f128b5f
Justin Voelkel <justin.voelkel@sourcetoad.com>
2024-11-22 12:33:48 -0500

    trying to add some transitions and updating navigation

--- components/BlogRoll.vue
+++ components/BlogRoll.vue
@@ -2,7 +2,7 @@
 import type {ParsedContent} from '@nuxt/content';
 import {useIntersectionObserver} from '@vueuse/core';
 
-const perPage = 10;
+const perPage = 8;
 const page = ref<number>(1);
 const posts = ref<ParsedContent[]>([]);
 const trigger = ref<HTMLElement | null>(null);
@@ -39,12 +39,37 @@ async function fetchMore() {
         v-for="post in posts"
         :key="post._id"
       >
-        <h2>{{ post.title }}</h2>
-        <p>{{ post.date }}</p>
-        <p>{{ post.excerpt }}</p>
-        <nuxt-link :to="post._path">Read more</nuxt-link>
+        <Transition
+          name="fade"
+          appear
+        >
+          <section>
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.date }}</p>
+            <p>{{ post.excerpt }}</p>
+            <nuxt-link :to="post._path">Read more</nuxt-link>
+          </section>
+        </Transition>
       </li>
       <span ref="trigger" />
     </ul>
   </main>
 </template>
+
+<style scoped>
+/* Transition for fade-in effect */
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    opacity 0.6s ease,
+    transform 0.6s ease;
+}
+.fade-enter-from {
+  opacity: 0;
+  transform: translateY(20px);
+}
+.fade-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
+import {navigationMenuTriggerStyle} from './ui/navigation-menu';
+import NavigationMenu from './ui/navigation-menu/NavigationMenu.vue';
+
 type NavigationItem = {
   title: string;
   to: string;
 };
 
 const navigation: NavigationItem[] = [
-  {title: 'Home', to: '/'},
+  {title: 'Posts', to: '/'},
   {title: 'Resume', to: '/resume'},
 ];
 </script>
 
 <template>
   <nav>
-    <ul>
-      <li
-        v-for="item in navigation"
-        :key="item.to"
-      >
-        <NuxtLink :to="item.to">{{ item.title }}</NuxtLink>
-      </li>
-    </ul>
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem
+          v-for="item in navigation"
+          :key="item.to"
+        >
+          <NuxtLink :to="item.to">
+            <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+              {{ item.title }}
+            </NavigationMenuLink>
+          </NuxtLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   </nav>
 </template>
